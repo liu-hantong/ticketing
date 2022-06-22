0b6f9c68ce058741d07198ab763d1a15159d963e
llht <hantongliu@outlook.com>
2022-06-22 13:29:33 +0800

    update settings for public deploy

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -4,8 +4,7 @@ const BuildClient = ({ req }) => {
   if (typeof window === 'undefined') {
     // we are on the server
     return axios.create({
-      baseURL:
-        'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local',
+      baseURL: 'http://www.ticketing-hantong.xyz/',
       headers: req.headers,
     });
   } else {
